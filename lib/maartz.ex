3f196b5814fca6a3b7f8c9cfe74848514c510bce
Sean Callan <seancallan@gmail.com>
2020-04-19 15:00:44 -0600

    Initial project

---
+++ lib/maartz.ex
@@ -0,0 +1,18 @@
+defmodule Maartz do
+  @moduledoc """
+  Documentation for `Maartz`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Maartz.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/maartz_test.exs
@@ -0,0 +1,8 @@
+defmodule MaartzTest do
+  use ExUnit.Case
+  doctest Maartz
+
+  test "greets the world" do
+    assert Maartz.hello() == :world
+  end
+end
