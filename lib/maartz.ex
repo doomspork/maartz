090bd67b3028098aa91ab293b59050567dcafd75
Sean Callan <seancallan@gmail.com>
2020-06-08 16:28:21 -0600

    Amend to work for Beansbooks API docs

--- lib/maartz.ex
+++ lib/maartz.ex
@@ -5,7 +5,7 @@ defmodule Maartz do
 
   alias Maartz.Storage
 
-  @url "https://www.carrefour.fr"
+  @url "https://beansbooks.com/api/action/Beans_Account_Calibrate"
 
   def run, do: walk(@url)
 
@@ -27,17 +27,114 @@ defmodule Maartz do
   defp internal_link_reducer({_tag_name, attributes, _children}, acc) do
     links =
       Enum.flat_map(attributes, fn
-        {"href", "/" <> _internal_path = path} -> [@url <> path]
+        {"href", "/api/object" <> _internal_path} -> []
+        {"href", "/api/" <> internal_path} -> [@url <> internal_path]
         _no_match -> []
       end)
 
     links ++ acc
   end
 
-  defp product_information(document) do
-    with title when not is_nil(title) <- get_tag(document, "h1.pdp-card__title"),
-         price when not is_nil(price) <- get_tag(document, ".product-card-price__price--final") do
-      IO.puts("#{title} - #{price}")
+  defp parse_description({_tag, _attrs, child_nodes}) do
+    nodes =
+      case List.last(child_nodes) do
+        {"table", [], []} ->
+          {_, new_nodes} = List.pop_at(child_nodes, -1)
+          new_nodes
+
+        other ->
+          other
+      end
+
+    case nodes do
+      [{_, [{"class", "type"}], [type]}] ->
+        {type, "", []}
+
+      [{_, [{"class", "type"}], [type]}, desc] ->
+        {type, String.trim(desc), []}
+
+      [
+        {_, [{"class", "type"}], [type]},
+        start,
+        {"a", _attrs, [resource]},
+        ending
+      ] ->
+        {type, String.trim("#{start}#{resource}#{ending}"), []}
+
+      [{_, [{"class", "type"}], [type]}, title, nested_nodes] ->
+        nodes = Floki.find(nested_nodes, "tr")
+        {type, String.trim(title), build_table(nodes, "  *")}
+
+      [desc] ->
+        {"", desc, []}
+    end
+  end
+
+  defp parse_row({_tag, _attrs, [name, description]}, prefix) do
+    {requirement, field} =
+      case name do
+        {_, [{_, field_requirement}], [field]} -> {field_requirement, field}
+        {_, _, [field]} -> {"", field}
+      end
+
+    required = if requirement == "", do: "", else: " (#{requirement})"
+    {type, desc, nested_params} = parse_description(description)
+
+    "#{prefix} #{field}#{required} #{type} - #{String.trim(desc)}\n#{nested_params}"
+  end
+
+  defp build_table(content, prefix \\ "*") do
+    content
+    |> Enum.map(&parse_row(&1, prefix))
+    |> Enum.join("\n")
+  end
+
+  defp parse_parameters(document) do
+    case Floki.find(document, "h3 + table") do
+      [{_, _, parameters}, {_, _, results}] ->
+        {build_table(Floki.find(parameters, "tr")), build_table(Floki.find(results, "tr"))}
+
+      [{_, _, parameters}] ->
+        {build_table(Floki.find(parameters, "tr")), nil}
+
+      _other ->
+        [{_, _, parameters}] = Floki.find(document, "table")
+        {build_table(Floki.find(parameters, "tr")), nil}
+    end
+  end
+
+  defp markdown_doc(title, description, parameters, nil) do
+    """
+    # #{title}
+    #{description}
+    ***
+    ## Attributes
+    #{parameters}
+    """
+  end
+
+  defp markdown_doc(title, description, parameters, results) do
+    """
+    # #{title}
+    #{description}
+    ***
+    ## Parameters
+    #{parameters}
+    *** 
+    ## Results
+    #{results}
+    """
+  end
+
+  defp api_doc(document) do
+    with title <- get_tag(document, "h2"),
+         description <- get_tag(document, "p.lead"),
+         {parameters, results} <- parse_parameters(document) do
+      filename = String.downcase("docs/#{title}.md")
+      markdown = markdown_doc(title, description, parameters, results)
+      File.write(filename, markdown)
+    else
+      err -> IO.inspect(err, label: :error)
     end
   end
 
@@ -47,7 +144,9 @@ defmodule Maartz do
         with true <- Storage.put(url),
              {:ok, %{body: body}} <- HTTPoison.get(url),
              {:ok, document} <- Floki.parse_document(body) do
-          product_information(document)
+          IO.inspect(url)
+
+          api_doc(document)
           walk_internal_links(document)
         end
       end)
